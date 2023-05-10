629b84309b77387012625e2de08eedac17c3c7c2
flowsar <brahim.sar151@gmail.com>
2023-05-10 23:56:22 +0100

    cycle

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,6 +5,20 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 int check_cycle(listint_t *list);
 
 #endif
