ee63060e261a16efb3026f569989833002f4b169
flowsar <brahim.sar151@gmail.com>
2023-05-10 22:24:49 +0100

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.split(", ")[2] + " with " + str.split(", ")[0])
+print(str[39:66] + " with " + str[0:6])
