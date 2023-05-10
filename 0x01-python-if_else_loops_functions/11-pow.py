97cfbb9c5b10fa39fef6c154ba6c890104fbf1c3
flowsar <brahim.sar151@gmail.com>
2023-05-10 21:28:58 +0100

    update

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def pow(a, b):
-	x = a
-	for i in range(1,b):
-		x *= a
-	return x
+    x = a
+    pow = b
+    if b < 0:
+        pow =-pow
+    if b == 0:
+    	return 1
+    for i in range(1,pow):
+       x *= a
+    if b < 0:
+        return 1/x
+    return x
