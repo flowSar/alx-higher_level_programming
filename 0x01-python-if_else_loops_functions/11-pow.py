195688c0beb38535b54f4657579ab5d3e6f8302c
flowsar <brahim.sar151@gmail.com>
2023-05-11 00:58:31 +0100

    fixx buzz

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
     x = a
-    pow = b
+    p = b
     if b < 0:
-        pow =-pow
+        p =-p
     if b == 0:
     	return 1
-    for i in range(1, pow):
+    for i in range(1, p):
        x *= a
     if b < 0:
         return 1/x
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python3
 def fizzbuzz():
-	for i in range(100):
-		print(i)
+    text = ""
+    for i in range(1, 100):
+        if i % 3 == 0:
+            text += "Fizz "
+        elif i % 5 == 0:
+            text += "Buzz "
+        elif i % 5 == 0 and i % 3 == 0:
+            text +="FizzBuzz "
+        else:
+            text += str(i)+" "
+    print(text+"$", end='')
+fizzbuzz()
