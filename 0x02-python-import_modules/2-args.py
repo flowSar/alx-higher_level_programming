da03acb06e42e151e265c21c8ee0565cf860d779
flowsar <brahim.sar151@gmail.com>
2023-05-11 23:19:37 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    import sys as syst
 
-    argc = len(sys.argv)
+    argc = len(syst.argv)
     if argc == 1:
         print("0 arguments.")
     else:
         print("{} arguments:".format(argc-1))
         for arg in range(1, argc):
-            print("{}: {}".format(arg, sys.argv[arg]))
+            print("{}: {}".format(arg, syst.argv[arg]))
