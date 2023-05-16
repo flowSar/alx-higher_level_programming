61c6d0c64d21cdc4bb7d34966172e6497627c303
flowsar <brahim.sar151@gmail.com>
2023-05-16 23:40:18 +0100

    update

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     
     if len(tuple_b) == 1 and len(tuple_a) != 1:
-    	new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
     elif len(tuple_b) == 1 and len(tuple_a) == 1:
         new_tuple = (tuple_a[0] + tuple_b[0], 0 + 0)
     elif len(tuple_a) > 1 and len(tuple_a) == 1:
@@ -14,6 +14,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         new_tuple = (0 + tuple_b[0], 0 + tuple_b[1])
     elif len(tuple_a) != 0 and len(tuple_b) == 0:
         new_tuple = (tuple_a[0] + 0, tuple_a[1] + 0)
+    elif len(tuple_a) == 0 and len(tuple_b) == 0:
+        new_tuple = (0, 0)
     else:
         new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return (new_tuple)
