6fd7d365ad65eb4a22d062217bcac69ea3b09b73
flowsar <brahim.sar151@gmail.com>
2023-05-16 23:15:31 +0100

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,16 +4,16 @@ def print_matrix_integer(matrix=[[]]):
     length = len(matrix)
     length_in = len(matrix[0])
     if length_in == 1:
-    	print("{:d}".format(matrix[0][0]))
-    	return
+        print("{:d}".format(matrix[0][0]))
+        return
     if len(matrix) == 1:
-    	print("{}".format(""))
-    	return
+        print("{}".format(""))
+        return
     for lst in matrix:
-    	i = 0
-    	for etm in lst:
-    		print("{:d}".format(etm), end='')
-    		if i < len(lst) -1:
-    			print("{}".format(" "), end = '')
-    		i += 1
-    	print("{}".format(""))
+        i = 0
+        for etm in lst:
+            print("{:d}".format(etm), end='')
+            if i < len(lst) -1:
+                print("{}".format(" "), end = '')
+            i += 1
+        print("{}".format(""))
