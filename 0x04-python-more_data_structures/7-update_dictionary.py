9174bcacb6dc9375fb5a5041a8268ffac19d6eb4
flowsar <brahim.sar151@gmail.com>
2023-05-17 23:36:55 +0100

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    nfount = 1
+    for od_key in a_dictionary:
+        if key == od_key:
+    	    a_dictionary[od_key] = value
+    	    nfount = 0
+        else:
+    	    a_dictionary[od_key] = a_dictionary[od_key]
+
+    if nfount == 1:
+        a_dictionary[key] = value
+    return a_dictionary
