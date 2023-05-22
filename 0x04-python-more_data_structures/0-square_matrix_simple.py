4d8fdb7501794fd0097e95a75f71da458ff204dd
flowsar <brahim.sar151@gmail.com>
2023-05-22 18:05:27 +0100

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,6 +6,6 @@ def square_matrix_simple(matrix=[]):
     new_matrix = [[0 for _ in range(len_in)] for _ in range(length)]
 
     for i in range(length):
-    	for j in range(len_in):
-    		new_matrix[i][j] = matrix[i][j] * matrix[i][j]
+        for j in range(len_in):
+            new_matrix[i][j] = matrix[i][j] * matrix[i][j]
     return new_matrix
