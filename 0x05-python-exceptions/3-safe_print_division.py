b3fe357a31866a1eacc0ef6ed8c3697c6f44024e
flowsar <brahim.sar151@gmail.com>
2023-05-29 23:18:16 +0100

    use finally with try/exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+	result = 0
+	try:
+		result = a / b
+		print("Inside result: {}".format(result))
+	except Exception as e:
+		print("Inside result: {}".format(None))
+	finally:
+		if (result == 0):
+			return None
+		return result
