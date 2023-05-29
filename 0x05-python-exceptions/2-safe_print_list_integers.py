3ea2b7b470f7f990c74cd07500805f001ae3127f
flowsar <brahim.sar151@gmail.com>
2023-05-29 23:10:28 +0100

    two diffrent exception handled

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    length = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            length += 1
+        except ValueError as e:
+        	continue
+        except TypeError as ty:
+            continue
+    print("")
+    return length
