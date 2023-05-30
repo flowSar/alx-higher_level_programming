3c81a238786d07721cc690652b835c5c55d29e10
flowsar <brahim.sar151@gmail.com>
2023-05-30 23:18:03 +0100

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-	this module have only one class
+this module have only one class
 """
 class Square:
 	"""
-		Square if an empty class
+	Square if an empty class
 	"""
     def __init__(self):
         """
