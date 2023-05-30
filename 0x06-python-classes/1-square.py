c10e1e10005d3ccc70d8786421ad226d65e1e1be
flowsar <brahim.sar151@gmail.com>
2023-05-30 23:37:55 +0100

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,11 +6,12 @@ In this module has one empty class names Square
 
 class Square:
 	"""
-	this is class named square
+	this is class named 'square'
+	has a private attribute names '_size'
 	"""
     _size = None
     def __init__(self, size=0):
     	"""
-    	Instantiate _size attribute with size 
+    	Instantiate '_size' attribute with 'size' 
     	"""
         self._size = size
