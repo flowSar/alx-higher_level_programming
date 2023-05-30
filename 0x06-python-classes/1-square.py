436d74ae125724d606b4ce02c96135ff489b911e
flowsar <brahim.sar151@gmail.com>
2023-05-30 23:24:46 +0100

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,12 @@ In this module has one empty class names Square
 """
 
 class Square:
+	"""
+	private variable
+	"""
     _size = None
     def __init__(self, size=0):
+    	"""
+    	set value to private variable
+    	"""
         self._size = size
