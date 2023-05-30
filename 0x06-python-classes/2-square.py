5a3f1aa6eb11efec26343d30c84516071c8c2b7e
flowsar <brahim.sar151@gmail.com>
2023-05-31 00:01:54 +0100

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Square:
-    _size = None
+    __size = None
     def __init__(self, size=0):
-        self._size = size
+        self.__size = size
         if not isinstance(size, int):
         	raise TypeError("size must be an integer")
         elif size < 0:
