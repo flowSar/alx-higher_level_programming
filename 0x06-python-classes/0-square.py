f080f3a4a518aca0fcb2978b5578c7202f0c978e
flowsar <brahim.sar151@gmail.com>
2023-05-30 23:16:28 +0100

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
+"""
+	this module have only one class
+"""
 class Square:
-"""
-	Square if an empty class
-"""
+	"""
+		Square if an empty class
+	"""
     def __init__(self):
         """
         __init__ is an empty init function
