aa349b4d4478bbf6219077a5ce9f107794ce9acf
flowsar <brahim.sar151@gmail.com>
2023-05-31 23:10:18 +0100

    style

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,20 +3,23 @@
 This is module-level documentation
 In this module we define a Square
 """
+
+
 class Square:
-    """ this is the class Square.
+
+    """
+    this is the class Square.
     __size is private attribute.
     """
+
     def __init__(self, size=0):
-        """
-        Instantiate '_size' attribute with size.
-        """
+        """Instantiate '_size' attribute with size."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-        	self.size = size
+            self.size = size
 
     def area(self):
         """
@@ -28,7 +31,7 @@ class Square:
 
     @property
     def size(self):
-    	return self.__size
+        return self.__size
 
     @size.setter
     def size(self, value):
