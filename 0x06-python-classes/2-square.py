a85a973c849f608eea198e50a8680acc8fdc467c
flowsar <brahim.sar151@gmail.com>
2023-05-31 19:58:44 +0100

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,27 +4,23 @@ This is module-level documentation
 In this module we define a Square
 """
 class Square:
+    """ this is the class Square.
+    __size is private attribute.
     """
-    This is a'Square' with private variable '_size'
-    """
-
-
     __size = None
     def __init__(self, size=0):
         """
-        Instantiate '_size' attribute with size
+        Instantiate '_size' attribute with size.
         """
-
         self.__size = size
         if not isinstance(size, int):
             """
-            we raise exception TypeError if size isn't int 
+            raise exception TypeError if size isn't int.
             """
-
-        	raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             """
-            we raise exception ValueError if size less than 0 
+            raise exception ValueError if size less than 0.
             """
-            
+
             raise ValueError("size must be >= 0")
