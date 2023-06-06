27fb8a8d624b283ccaaa3471c7a5e7b65c6bb7d6
flowsar <brahim.sar151@gmail.com>
2023-06-06 19:36:25 +0100

    readme

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -86,17 +86,3 @@ class Rectangle:
             if i < self.height - 1:
                 shape += "\n"
         return shape
-                
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
