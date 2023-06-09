70d10ae9d6ca662e0446d4d72336cb6ed7bba564
flowsar <brahim.sar151@gmail.com>
2023-06-09 18:32:56 +0100

    update

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" function for adding two integers"""
+def add_integer(a, b=98):
+    try:
+    	a = int(a)
+    except Exception as e:
+    	return "a must be an integer"
+
+    try:
+    	b = int(b)
+    except Exception as e:
+    	return "b must be an integer"
+    return a + b
