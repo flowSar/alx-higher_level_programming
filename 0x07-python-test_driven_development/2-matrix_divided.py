00f448d9e03dfe39d21c9e50296903ed706755dd
flowsar <brahim.sar151@gmail.com>
2023-06-09 19:07:20 +0100

    update

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    """ function divides all elements of a matrix"""
+    new_matrix = []
+    inside_matrix = []
+    for mat in matrix:
+    	for elm in mat:
+    		value = format(elm/3, ".2f")
+    		inside_matrix += [float(value)]
+    	new_matrix += [inside_matrix]
+    	inside_matrix = []
+
+    return new_matrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(matrix_divided(matrix, 3))
+print(matrix)
+   
