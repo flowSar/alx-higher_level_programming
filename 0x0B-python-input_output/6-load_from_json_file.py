cc2696da4ee678440bdff607470643dc824ac8a7
flowsar <brahim.sar151@gmail.com>
2023-06-13 23:47:26 +0100

    update

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is module-level documentation
+In this module has one function names load_from_json_file.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    this function dump a dictioney object to a file 
+    Args:
+        filename: filename or a path of a file.
+    """
+    #json_data = ""
+    with open(filename, "r") as f:
+        json_data = f.read()
+
+    json_object = json.loads(json_data)
+    return json_object
