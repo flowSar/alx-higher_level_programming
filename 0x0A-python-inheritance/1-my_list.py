9bfaab1bbb7b2593bf0337926bd60dd3b0a5136a
flowsar <brahim.sar151@gmail.com>
2023-06-13 17:16:25 +0100

    documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+This is module-level documentation
+In this module has one class names MyList.
+"""
 class MyList(list):
     """MyList is a subclass of list"""
     def print_sorted(self):
