380fa18155404317b0dc484894dfb2c669974f7a
flowsar <brahim.sar151@gmail.com>
2023-08-16 23:22:34 +0100

    remove

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def no_c(my_string):
-    new = ""
-    for el in my_string:
-        if el != 'c' and el != 'C':
-            new += el
-    return new
--- 0x03-python-data_structures/8-multiple_returns.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-def multiple_returns(sentence):
-    length = len(sentence)
-    return (length, sentence[:1])
