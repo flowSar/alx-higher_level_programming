c9d27d092cdb3e6f517efab2d2344bfe5b3e8afc
flowsar <brahim.sar151@gmail.com>
2024-04-10 21:05:42 +0000

    fix python style

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
+
 def matrix_divided(matrix, div):
+
     """ function divides all elements of a matrix"""
+
     new_matrix = []
     inside_matrix = []
     for mat in matrix:
-    	for elm in mat:
-    		value = format(elm/3, ".2f")
-    		inside_matrix += [float(value)]
-    	new_matrix += [inside_matrix]
-    	inside_matrix = []
+        for elm in mat:
+            value = format(elm/3, ".2f")
+            inside_matrix += [float(value)]
+        new_matrix += [inside_matrix]
+        inside_matrix = []
 
     return new_matrix
-   
