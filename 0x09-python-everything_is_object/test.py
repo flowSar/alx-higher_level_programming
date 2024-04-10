01b3572b3dd0d24b5b9294618ace858c132a895e
flowsar <brahim.sar151@gmail.com>
2024-04-10 02:14:13 +0000

    upload projects

---
+++ 0x09-python-everything_is_object/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class User:
+
+	def __init__(self):
+		pass
+
+	def increment(x, y):
+		x.append(0)
+		x = y
+
+"""
+helli
+"""
+if __name__ == '__main__':
+	
+	a = (1, )
+	a = (1)
+	b = (1)
+	a is b
