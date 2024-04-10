ee77254e3dd89976b17c6b95b2f584f134b4d9df
flowsar <brahim.sar151@gmail.com>
2024-04-10 20:33:42 +0000

    fix pythonstype

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
 """ function for adding two integers"""
+
+
 def add_integer(a, b=98):
-    try:
-    	a = int(a)
-    except Exception as e:
-    	return "a must be an integer"
 
     try:
-    	b = int(b)
+        a = int(a)
     except Exception as e:
-    	return "b must be an integer"
+        return "a must be an integer"
+
+    try:
+        b = int(b)
+    except Exception as e:
+        return "b must be an integer"
     return a + b
