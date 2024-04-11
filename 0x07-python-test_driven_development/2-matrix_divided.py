7c9f949a47cfc87b970230d8842d4abecf9a3520
flowsar <brahim.sar151@gmail.com>
2024-04-11 00:39:51 +0000

    add docu

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -23,7 +23,7 @@ def matrix_divided(matrix, div):
     if not isinstance(div, int | float):
         raise TypeError("div must be a number")
     if div == 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError("division by zero ")
 
     for mat in matrix:
         for elm in mat:
