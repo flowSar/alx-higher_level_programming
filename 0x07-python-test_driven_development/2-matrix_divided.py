8fbb6824f71e0b92e80ff33fa06aae634b92cc02
flowsar <brahim.sar151@gmail.com>
2024-04-11 00:26:21 +0000

    old version

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,40 +1,15 @@
 #!/usr/bin/python3
 
-
 def matrix_divided(matrix, div):
-
-    """
-        function divides all elements of a matrix bby div
-
-        Args:
-            matrix: matrix
-            div: div
-
-        Return:
-            if all goes will return matrix/div
-    """
-
+    """ function divides all elements of a matrix"""
     new_matrix = []
     inside_matrix = []
-    first_row = len(matrix[0])
-    row_number = 0
-    if not isinstance(div, int | float):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
     for mat in matrix:
-        for elm in mat:
-            row_number += 1
-            if not isinstance(elm, int | float):
-                raise TypeError("""matrix must be a matrix"""
-                                """(list of lists) of integers/floats""")
-            value = format(elm/div, ".2f")
-            inside_matrix += [float(value)]
-        if row_number != first_row:
-            raise TypeError("Each row of the matrix must have the same size")
-        row_number = 0
-        new_matrix += [inside_matrix]
-        inside_matrix = []
+    	for elm in mat:
+    		value = format(elm/3, ".2f")
+    		inside_matrix += [float(value)]
+    	new_matrix += [inside_matrix]
+    	inside_matrix = []
 
     return new_matrix
+   
