02eb36700f8873838bab3f320c2c2bc1dd7929dd
flowsar <brahim.sar151@gmail.com>
2024-04-11 01:23:10 +0000

    update

--- 0x09-python-everything_is_object/test.py
+++ 0x09-python-everything_is_object/test.py
@@ -14,10 +14,6 @@ helli
 """
 if __name__ == '__main__':
 	
-	def assign_value(n, v):
-	    n = v
-
-	l1 = [1, 2, 3]
-	l2 = [4, 5, 6]
-	assign_value(l1, l2)
-	print(l1)
+	a = (1)
+	b = (1)
+	print(a is b)
