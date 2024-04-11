e9d8af17d1c0eedaf9dfe2120ffd01498c934741
flowsar <brahim.sar151@gmail.com>
2024-04-11 00:19:11 +0000

    update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -19,20 +19,20 @@ def matrix_divided(matrix, div):
     first_row = len(matrix[0])
     row_number = 0
     if not isinstance(div, int | float):
-        raise TypeError("div must be a number\n")
+        raise TypeError("div must be a number")
     if div == 0:
-        raise ZeroDivisionError("division by zero\n")
+        raise ZeroDivisionError("division by zero")
 
     for mat in matrix:
         for elm in mat:
             row_number += 1
             if not isinstance(elm, int | float):
                 raise TypeError("""matrix must be a matrix"""
-                                """(list of lists) of integers/floats\n""")
+                                """(list of lists) of integers/floats""")
             value = format(elm/div, ".2f")
             inside_matrix += [float(value)]
         if row_number != first_row:
-            raise TypeError("Each row of the matrix must have the same size\n")
+            raise TypeError("Each row of the matrix must have the same size")
         row_number = 0
         new_matrix += [inside_matrix]
         inside_matrix = []
