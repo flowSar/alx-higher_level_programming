b3e74f557506e029c0abac12c96b7100ce75b4b7
flowsar <brahim.sar151@gmail.com>
2024-04-12 22:26:38 +0000

    update code

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -21,20 +21,20 @@ def matrix_divided(matrix, div):
     first_row = len(matrix[0])
     row_number = 0
     if not isinstance(div, int | float):
-        raise TypeError("div must be a number")
+        raise TypeError("div must be a number\n")
     if div == 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError("division by zero\n")
 
     for mat in matrix:
         for elm in mat:
             row_number += 1
             if not isinstance(elm, int | float):
                 raise TypeError("""matrix must be a matrix"""
-                                """(list of lists) of integers/floats""")
+                                """(list of lists) of integers/floats\n""")
             value = format(elm/div, ".2f")
             inside_matrix += [float(value)]
         if row_number != first_row:
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError("Each row of the matrix must have the same size'n")
         row_number = 0
         new_matrix += [inside_matrix]
         inside_matrix = []
--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -3,8 +3,6 @@
 
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [
-    [3]
-]
-print(matrix_divided(matrix, 3))
+matrix = [[3, 8], [12, 11]]
+print(matrix_divided(matrix, -3))
 print(matrix)
