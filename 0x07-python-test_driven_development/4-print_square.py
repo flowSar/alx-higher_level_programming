82779d6d5147cae6b841e4ee42b9e49efd155640
flowsar <brahim.sar151@gmail.com>
2024-04-12 23:17:27 +0000

    update

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,11 +2,13 @@
 
 
 def print_square(size):
+
     """
         function print square of #
         Args:
             size: size of squre
     """
+
     if size < 0:
         raise ValueError("size must be >= 0")
     elif type(size) is float and size < 0:
