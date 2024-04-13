44ff86d3fc0748153b4d72b9c22952f85cd5878f
flowsar <brahim.sar151@gmail.com>
2024-04-13 00:55:02 +0000

    update

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 def max_integer(list=[]):
     """Function to find and return the max integer in a list of integers
     If the list is empty, the function returns None
@@ -20,7 +21,7 @@ def max_integer(list=[]):
 class TestMaxInteger(unittest.TestCase):
 
     def test_positive_numbers(self):
-        self.assertEqual(max_integer([1,4,5,6,7,0]), 7)
+        self.assertEqual(max_integer([1, 4, 5, 6, 7, 0]), 7)
         self.assertEqual(max_integer([100, 4, 300, 4, 0, 2]), 300)
 
     def test_negative_numbers(self):
@@ -30,6 +31,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_enmpty_lest(self):
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([0, -1, -2, -3]), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
