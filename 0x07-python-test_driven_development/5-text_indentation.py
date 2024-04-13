9e237eb210ca49b71d786088eefbb33a8115560d
flowsar <brahim.sar151@gmail.com>
2024-04-13 00:17:02 +0000

    fix space problem

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,11 +3,23 @@
 def text_indentation(text):
     """function print a text as lines witch each line end with '.' or '':'"""
     found = 0
+    space = 0
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
     for letter in text:
+        if letter == ' ':
+            space +=1
+        if letter != ' ':
+            space = 0
         if found == 1:
             found = 0
+            space += 1
+            if space <=1:
+                print(letter, end='')
             continue
-        print(letter, end='')
-        if letter == '.' or letter == ':':
+        if space <=1:
+            print(letter, end='')
+        if letter == '.' or letter == ':' or letter == '?':
             found += 1
             print("\n")
