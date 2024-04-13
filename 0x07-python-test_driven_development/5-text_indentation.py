4a6482aefbd67046f84551a97af81d3cfc954f5d
flowsar <brahim.sar151@gmail.com>
2024-04-13 00:23:27 +0000

    fix pycodestyle

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 
+""" functio split text into lines when encounter '. or ':' or '?' """
+
+
 def text_indentation(text):
-    """function print a text as lines witch each line end with '.' or '':'"""
+
+    """
+
+        function print a text as lines witch each line end
+        with '.' or '':'" or '?'
+
+        Args:
+            text: this text that we handle in this function
+    """
+
     found = 0
     space = 0
 
@@ -9,16 +21,16 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     for letter in text:
         if letter == ' ':
-            space +=1
+            space += 1
         if letter != ' ':
             space = 0
         if found == 1:
             found = 0
             space += 1
-            if space <=1:
+            if space <= 1:
                 print(letter, end='')
             continue
-        if space <=1:
+        if space <= 1:
             print(letter, end='')
         if letter == '.' or letter == ':' or letter == '?':
             found += 1
