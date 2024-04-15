7c6acf406f8fe2c21ab9c2bef3b451599f2bc03a
flowsar <brahim.sar151@gmail.com>
2024-04-16 00:59:25 +0100

    up

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -20,8 +20,10 @@ class Rectangle(BaseGeometry):
     __height = 0
 
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
 
 r = Rectangle(3, 5)
 
