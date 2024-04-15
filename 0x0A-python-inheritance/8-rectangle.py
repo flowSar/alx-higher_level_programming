990d51114107186ad8520ca5624f0f0be1eb0999
flowsar <brahim.sar151@gmail.com>
2024-04-16 00:49:47 +0100

    up

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,7 +6,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
     and he's a subclass on BaseGeometry class
 """
 
-
 class Rectangle(BaseGeometry):
 
     """
@@ -20,5 +19,20 @@ class Rectangle(BaseGeometry):
     __height = 0
 
     def __init__(self, width, height):
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
+
+try:
+    print("Rectangle: {} - {}".format(r.width, r.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
