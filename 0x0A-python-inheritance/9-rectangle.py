960fa49908e7de23b083dee7ad2371fe75a8f8bf
flowsar <brahim.sar151@gmail.com>
2024-04-16 00:22:05 +0100

    pycodestyle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
 
     """Rectangle is a subclass of BaseGeometry"""
     __width = 0
     __height = 0
+
     def __init__(self, width, height):
         if type(width) is not int:
             raise TypeError("width must be an integer")
