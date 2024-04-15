7d4078f6b62071a73ac177ede7e9c099c2ba232e
flowsar <brahim.sar151@gmail.com>
2024-04-15 19:47:38 +0100

    code style

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,7 +6,15 @@ In this module has one function names is_same_class.
 
 
 def is_same_class(obj, a_class):
-    """function return true if obj is instance of a_class and false If not"""
+    """
+    function check if obj is instance of a_class and return True it it's
+    and False if not
+
+    Args:
+        obj: object 1
+        a_class: object 2
+
+    """
     if isinstance(obj, a_class):
         return True
     return False
