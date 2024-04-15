faffe7ba6fea0f19db8f0970e148a922227833f2
flowsar <brahim.sar151@gmail.com>
2024-04-16 00:43:43 +0100

    update

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+    this module continue a Rectangle class  with constractor method
+    and he's a subclass on BaseGeometry class
+"""
 
 
 class Rectangle(BaseGeometry):
 
-    """Rectangle is a subclass of BaseGeometry"""
+    """
+        Rectangle is a subclass of BaseGeometry
+        Attribute:
+            __width: width of Rectange
+            __height: height of Rectangle
+
+    """
     __width = 0
     __height = 0
 
     def __init__(self, width, height):
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        self.__width = width
-        if type(height)is not int:
-            raise TypeError("height must be an integer")
-        self.__height = height
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
