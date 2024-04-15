cca97504bba6aa9b1e124fcf0b37a6007773793c
flowsar <brahim.sar151@gmail.com>
2024-04-15 21:53:00 +0100

    update code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
+"""this module have a function for checking if the object
+    is an inheritence of a class """
+
 
 def inherits_from(obj, a_class):
-    """function return true if obj is instance of class that inherited"""
-    if a_class == bool:
-    	return False
-    return isinstance(obj, a_class)
+    """
+    function for checking if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class and
+    return True it it's and False if not
+
+    Args:
+        obj: object 1
+        a_class: object 2
+
+    Return: True if obj instance of a_class and False if not
+    """
+    if type(obj) != a_class:
+        if isinstance(obj, a_class):
+            return True
+    return False
