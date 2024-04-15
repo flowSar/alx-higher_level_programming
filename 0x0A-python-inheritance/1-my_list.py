78fd8c4749523b2b3817be36d276b00de014aeca
flowsar <brahim.sar151@gmail.com>
2024-04-15 19:16:53 +0100

    doc

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,7 +6,16 @@ In this module has one class names MyList.
 
 
 class MyList(list):
-    """MyList is a subclass of list"""
+    """
+        Print the elements of the list in sorted order
+        This method creates a copy of the list, sorts it in ascending order,
+        and then prints the sorted list.
+        Args:
+            None
+        Returns:
+            None
+
+    """
     def print_sorted(self):
         """sort a list and print it"""
         my_list = [elm for elm in self]
