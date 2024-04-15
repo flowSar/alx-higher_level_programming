dfc670201eba475d1aa1e4ce6c8abe13378c0ce6
flowsar <brahim.sar151@gmail.com>
2024-04-15 17:29:50 +0100

    python style fixed

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """
-This is module-level documentation
-In this module has one function names lookup.
+    this module provides a function that return a list of object attributes
+    and methodes
 """
+
+
 def lookup(obj):
-	"""function return list of attributes and methods of an object"""
-	return dir(obj)
+    """this function return list of attributes and methods of an object
+        Args:
+            obj: object
+        return:
+            list of object content
+    """
+    return dir(obj)
