94b5481f7982058d8484edb9afa8505ac00fa5e4
flowsar <brahim.sar151@gmail.com>
2024-04-15 18:50:50 +0100

    sort a list not build a function that sort it

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """
-In this module has one class which is a subclass of a list.
+This is module-level documentation
+In this module has one class names MyList.
 """
 
 
 class MyList(list):
-
+    """MyList is a subclass of list"""
     def print_sorted(self):
-        """ Sort list and print it """
-        new_List = [x for x in self]
-        length = len(new_List)
-        for i in range(length):
-            for j in range(length):
-                if new_List[i] < new_List[j]:
-                    tmp = new_List[i]
-                    new_List[i] = new_List[j]
-                    new_List[j] = tmp
-
-        print(new_List)
+        """sort a list and print it"""
+        my_list = [elm for elm in self]
+        my_list.sort()
+        print(my_list)
