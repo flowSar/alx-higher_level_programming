58f00e3eb5c96204df2fd0795bae26f1e5efc7d2
flowsar <brahim.sar151@gmail.com>
2024-04-15 19:28:26 +0100

    code style

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,10 @@
 This is module-level documentation
 In this module has one function names is_same_class.
 """
-def is_same_class(self, obj, a_class):
-    """function return true if obj is instance of a_class"""
-    return isinstance(obj, a_class)
+
+
+def is_same_class(obj, a_class):
+    """function return true if obj is instance of a_class and false If not"""
+    if isinstance(obj, a_class):
+        return True
+    return False
--- 0x0A-python-inheritance/test.py
+++ 0x0A-python-inheritance/test.py
@@ -1,25 +1,10 @@
 #!/usr/bin/python3
+is_same_class = __import__('2-is_same_class').is_same_class
 
-class Employee:
-
-    full_name = None
-    def __init__(self, f_name, l_name, phone):
-	    self.l_name = l_name
-	    self.f_name = f_name
-
-
-    def full_user_name(self):
-        self.full_name = self.f_name+" "+self.l_name
-        return self.full_name
-
-    def is_same_class(self, obj, a_class):
-        return isinstance(obj, a_class)
-
-
-if __name__ == '__main__':
-
-	em_1 = Employee("brahim", "sarouri", "0650704968")
-	em_1.full_user_name()
-	print(em_1.full_name)
-
-	print(em_1.is_same_class(em_1, Employee))
+a = 1
+if is_same_class(a, int):
+    print("{} is an instance of the class {}".format(a, int.__name__))
+if is_same_class(a, float):
+    print("{} is an instance of the class {}".format(a, float.__name__))
+if is_same_class(a, object):
+    print("{} is an instance of the class {}".format(a, object.__name__))
