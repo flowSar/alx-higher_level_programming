7e95d9fc0f5a5940d66269ba51d14e71e4ceaeee
flowsar <brahim.sar151@gmail.com>
2024-04-15 22:29:05 +0100

    style fixed

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+    this module have a class named as BaseGeometry and within this
+    there's an ares function that raise exception
+"""
+
 
 class BaseGeometry:
     """class named BaseGeometry """
     def area(self):
         """empty function raise Exception"""
-    	raise Exception("area() is not implemented")
+        raise Exception("area() is not implemented")
