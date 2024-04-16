d9926f523b8a485cadf0b69243f8340d3cad5c0d
flowsar <brahim.sar151@gmail.com>
2024-04-16 01:24:31 +0100

    update

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -25,9 +25,7 @@ class BaseGeometry:
                 TypeError: If the value is not an integer.
                 ValueError: If the value is less than or equal to 0.
         """
-        if value == True or value == False:
-            raise TypeError(f"{name} must be an integer")
-        elif not isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
