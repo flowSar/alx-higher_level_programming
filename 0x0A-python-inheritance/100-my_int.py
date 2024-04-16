dc7c1bf3873994f6a6ea20e64955913d9c5d3c31
flowsar <brahim.sar151@gmail.com>
2024-04-16 14:18:25 +0100

    doc

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+"""
+	this module has a class named MyInt and he's a subclass of superclass int
+"""
+
+class MyInt(int): 
+
+	static int
+	long_compare(PyLongObject *a, PyLongObject *b)
+		if (a == b)
+			return False
+
+
