60351335e18e8b2d89236a9ab31edc60c412c710
flowsar <brahim.sar151@gmail.com>
2024-04-16 14:45:43 +0100

    py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -16,6 +16,6 @@ def is_same_class(obj, a_class):
     Return: True if obj is instance of a_class and False if not
 
     """
-    if (type(obj) == a_class):
+    if (type(obj) is a_class):
         return True
     return False
