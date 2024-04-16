bad6089092326255d0c500892f36b0e8171a32cc
flowsar <brahim.sar151@gmail.com>
2024-04-16 01:41:06 +0100

    upload new modified code

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
+"""
+    this module continue a Rectangle class  with constractor method
+    and he's a subclass on BaseGeometry class
+"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
 
-    """Rectangle is a subclass of BaseGeometry"""
-    __width = 0
-    __height = 0
+    """
+        Rectangle is a subclass of BaseGeometry
+        Attribute:
+            __width: width of Rectange
+            __height: height of Rectangle
+
+    """
 
     def __init__(self, width, height):
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
         self.__width = width
-        if type(height)is not int:
-            raise TypeError("height must be an integer")
         self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
 
     def area(self):
-        """return the area of the Rectangle"""
+        """
+            this function is implementation of area function from superclass
+            Return:
+                return the area of rctangle
+        """
         return self.__width * self.__height
 
     def __str__(self):
+        """
+            this function is a magic function that print instance of this class
+        """
         return "[Rectangle] "+str(self.__width)+"/"+str(self.__height)
