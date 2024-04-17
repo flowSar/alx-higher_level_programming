b6dce2feed331db5c5761921c4de0e9e7a2b46a8
flowsar <brahim.sar151@gmail.com>
2024-04-17 02:39:55 +0100

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 """
-This is module-level documentation
+
 In this module has one function names load_from_json_file.
 """
 import json
+import sys
+
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+my_list = []
+
+if len(sys.argv) > 1:
+	for item in sys.argv[1:]:
+		my_list.append(item)
+
+print(my_list)
+
+with open("python_list.txt", "w") as file:
+	file.write("hello")
+
+loaded_data = load_from_json_file("python_list.txt")
+save_to_json_file(loaded_data, "add_item.json")
