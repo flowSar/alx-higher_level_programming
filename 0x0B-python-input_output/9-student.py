7f32d3623b307e2bfdfd864f291448a7f7a27f3a
flowsar <brahim.sar151@gmail.com>
2024-04-17 16:33:31 +0100

    create to_json fun

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,6 +1,33 @@
 #!/usr/bin/python3
 """
-This is module-level documentation
-In this module has one function names load_from_json_file.
+    his module contains a class Student that represent
+    student information and methods to handle student data.
 """
-import json
+
+
+class Student:
+    """
+    student with first name, last name, and age attributes.
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializes a new Student object. """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+            function return a student attribute as a dictionary
+
+            Return: return a dictionary containing the student's
+                first name, last name, and age.
+        """
+        students = {"first_name": self.first_name,
+                    "last_name": self.last_name,
+                    "age": self.age}
+        return students
