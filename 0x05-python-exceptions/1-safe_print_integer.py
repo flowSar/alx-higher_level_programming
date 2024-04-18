5683d91d4dcf0910d8627e813b199527b287a17a
flowsar <brahim.sar151@gmail.com>
2024-04-19 00:37:01 +0100

    fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,9 +2,8 @@
 
 
 def safe_print_integer(value):
-
-	try:
-		print("{:d}".format(value));
-		return True
-	except Exception as e:
-		return False
+    try:
+        print("{:d}".format(value));
+        return True
+    except Exception as e:
+        return False
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,18 +2,19 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-	try:
-		d_printed = 0
-		for i in range(x):
-			if (isinstance(my_list[i], int) != True):
-				continue
-			print("{:d}".format(my_list[i]), end = '')
-			d_printed += 1
-		print("")
-		return d_printed
-	except IndexError as e:
-		print("")
-		return d_printed
+    try:
+        d_printed = 0
+        for i in range(x):
+            if (isinstance(my_list[i], int) is not True):
+                continue
+            print("{:d}".format(my_list[i]), end='')
+            d_printed += 1
+        print("")
+        return d_printed
+    except IndexError as e:
+        print("")
+        return d_printed
 
-lis = [1,2,3,'g']
-print("{}".format(safe_print_list_integers(lis, 4)));
+
+lis = [1, 2, 3, 'g']
+print("{}".format(safe_print_list_integers(lis, 4)))
