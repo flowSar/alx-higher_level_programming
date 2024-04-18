c301fa595e40b4e117bfb9f3e9df1f86f357f5a7
flowsar <brahim.sar151@gmail.com>
2024-04-18 22:18:52 +0100

    fix exit stae

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     argc = len(syst.argv)
     if argc < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     else:
         if syst.argv[2] == '+':
             a = int(syst.argv[1])
