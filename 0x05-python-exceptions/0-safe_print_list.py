d3843378bb1d683e355ac9e79511fb7b5360534e
flowsar <brahim.sar151@gmail.com>
2024-04-19 00:32:20 +0100

    updat

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
-	p_num = 0
-	try:
-		for i in range(x):
-			p_num += 1
-			print(f"{my_list[i]}", end = '')
-		print("")
-		return p_num
-	except Exception as e:
-		return p_num
+
+    p_num = 0
+    try:
+        for i in range(x):
+            p_num += 1
+            print(f"{my_list[i]}", end='')
+        print("")
+        return p_num
+    except Exception as e:
+        return p_num
