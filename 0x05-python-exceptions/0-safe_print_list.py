71ca69fa19f2d428d097773f93b1f22d04acb6c7
flowsar <brahim.sar151@gmail.com>
2024-04-27 19:07:13 +0100

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,10 @@ def safe_print_list(my_list=[], x=0):
     p_num = 0
     try:
         for i in range(x):
-            p_num += 1
             print(f"{my_list[i]}", end='')
+            p_num += 1
         print("")
         return p_num
     except Exception as e:
+        print("")
         return p_num
