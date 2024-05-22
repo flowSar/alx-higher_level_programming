dc1b13dcb808a48064668f9f4edea18c19061dde
flowsar <brahim.sar151@gmail.com>
2024-05-22 19:17:32 +0100

    up

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- create table in databse
 
-CREATE database IF NOT EXISTS hbtn_0c_0;
-CREATE TABLE hbtn_0c_0.first_table (
+
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (
 id INTEGER,
 name VARCHAR(256)
 );
