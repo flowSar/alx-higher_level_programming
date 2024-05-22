f72110ebade9cb7e27227ea0c235f742d6c03363
flowsar <brahim.sar151@gmail.com>
2024-05-23 00:30:01 +0100

    create database.table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- create new database with table 
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+id INT DEFAULT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256)
+);
