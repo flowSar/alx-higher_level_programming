1b786145b96694c0c874e6b47afe66e0ffdf7bdb
flowsar <brahim.sar151@gmail.com>
2024-05-23 00:13:51 +0100

    update

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- create a new database and grant SELECT permmision to a new user
 
-CREATE DATABASE hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT SELECT ON hbtn_0d_2 TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
