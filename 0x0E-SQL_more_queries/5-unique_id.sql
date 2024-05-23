a10019fa615a3dc044ab774e3130bc6bb2d0e8c8
flowsar <brahim.sar151@gmail.com>
2024-05-24 00:27:59 +0100

    unique value

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- create a table and set default value (unique value) to it attribute
 
-CREATE TABLE IF NOT EXISTS id_not_null(
+CREATE TABLE IF NOT EXISTS unique_id(
 id INT DEFAULT 1 UNIQUE,
 name VARCHAR(256)
 );
