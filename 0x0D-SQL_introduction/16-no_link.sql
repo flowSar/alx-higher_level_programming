fe3d719512615b48f2d22e80f75b60d29ad0b9f7
flowsar <brahim.sar151@gmail.com>
2024-05-24 00:08:19 +0100

    selelct

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- retrieve from table and print in descending order by score
 
-SELECT score, name FROM second_table ORDER BY score DESC ;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC ;
