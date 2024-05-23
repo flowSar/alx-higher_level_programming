522263dffe7cb9c1597ba8ae24824c30e6b4a872
flowsar <brahim.sar151@gmail.com>
2024-05-24 00:36:52 +0100

    slect name id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,10 @@
 -- list all state of california
 
-SELECT * from states WHERE name='California' ORDER BY cities.id;
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY cities.id ASC;
