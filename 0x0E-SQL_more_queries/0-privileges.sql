050d6079db2e943a6ed6094041dee842b2969b89
flowsar <brahim.sar151@gmail.com>
2024-05-24 00:20:14 +0100

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- listing granted permession for users 
 
-SELECT * 
-FROM mysql.user 
-WHERE user IN ('user_0d_1', 'user_0d_2');
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
