f002ce92e5b29cd2e343f086274f2034f35b5eb9
flowsar <brahim.sar151@gmail.com>
2024-06-20 00:46:37 +0100

    square

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,2 +1,33 @@
 #!/usr/bin/node
 
+class Rectangle {
+  width;
+  height;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 
+const Rectangle = require('./4-rectangle.js');
+class Square extends Rectangle {
+  size;
+  constructor (size) {
+    super();
+    this.size = size;
+  }
+}
+
+module.exports = Square;
