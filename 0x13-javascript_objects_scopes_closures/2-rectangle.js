92cf939ed67613b811d22405bac5b421d7d3c246
flowsar <brahim.sar151@gmail.com>
2024-06-20 00:23:48 +0100

    rectangle class

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
 
+class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+    if (this.width <= 0 || this.height <= 0 || !this.height || !this.width) {
+      delete this.width;
+      delete this.height;
+    }
+  }
+}
+
+module.exports = Rectangle;
