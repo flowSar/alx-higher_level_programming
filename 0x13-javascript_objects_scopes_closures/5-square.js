4a20d7e16ef61dc453e4a8e1f28b0bd90c9b0260
flowsar <brahim.sar151@gmail.com>
2024-06-20 00:48:43 +0100

    fix constructor

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,9 +4,14 @@ const Rectangle = require('./4-rectangle.js');
 class Square extends Rectangle {
   size;
   constructor (size) {
-    super();
+    super(size, size);
     this.size = size;
   }
 }
 
-module.exports = Square;
+// module.exports = Square;
+
+const s1 = new Square(4);
+s1.print();
+s1.double();
+s1.print();
