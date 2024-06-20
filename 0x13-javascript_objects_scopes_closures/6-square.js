4d5df51261531ccd5304853352d3a7c932366aec
flowsar <brahim.sar151@gmail.com>
2024-06-20 01:15:45 +0100

    return number of repeated item in a list

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,2 +1,19 @@
 #!/usr/bin/node
 
+const square = require('./5-square.js');
+class Square extends square {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      let row = '';
+      for (let j = 0; j < this.size; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 
+exports.nbOccurences = function (list, searchElement) {
+  let repeated = 0;
+  for (let number of list) {
+    if (number === searchElement) {
+      repeated += 1;
+    }
+  }
+  return repeated;
+}
