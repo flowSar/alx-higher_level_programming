8872e5850bab88c814e368cec42c86a14254e1ac
flowsar <brahim.sar151@gmail.com>
2024-06-22 15:02:50 +0100

    select all from db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,15 @@
 #!/usr/local/bin/python3
+"""this module if for querying data from databbase """
+import MySQLdb
+import sys
+
+args = sys.argv[1:]
+
+db = MySQLdb.connect(host="localhost", port=3360, user=f"{args[0]}",
+                     passwd=f"{args[1]}", db=f"{args[2]}", charset="utf8")
+cursor = db.cursor()
+if __name__ == "__main__":
+    cursor.execute("select * from students ORDER BY student_id")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
