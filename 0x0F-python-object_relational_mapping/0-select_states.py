1aecd1d5c67c6d8748340a430cc0613c87f39d37
flowsar <brahim.sar151@gmail.com>
2024-06-22 15:04:56 +0100

    states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,11 +5,11 @@ import sys
 
 args = sys.argv[1:]
 
-db = MySQLdb.connect(host="localhost", port=3360, user=f"{args[0]}",
+db = MySQLdb.connect(host="localhost", port=3306, user=f"{args[0]}",
                      passwd=f"{args[1]}", db=f"{args[2]}", charset="utf8")
 cursor = db.cursor()
 if __name__ == "__main__":
-    cursor.execute("select * from students ORDER BY student_id")
+    cursor.execute("select * from states ORDER BY student_id")
     query_rows = cursor.fetchall()
     for row in query_rows:
         print(row)
