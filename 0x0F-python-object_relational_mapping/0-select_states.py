4142f12faf3cda57c7c15b34cf12c8fd9e7c17ca
flowsar <brahim.sar151@gmail.com>
2024-06-22 15:12:02 +0100

    my python is on local that why

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,9 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 """this module if for querying data from databbase """
 import MySQLdb
 import sys
 
 args = sys.argv[1:]
-
 db = MySQLdb.connect(host="localhost", port=3306, user=f"{args[0]}",
                      passwd=f"{args[1]}", db=f"{args[2]}", charset="utf8")
 cursor = db.cursor()
