261e883e8221575078cd1e62460f5711f7f700ff
flowsar <brahim.sar151@gmail.com>
2024-06-22 15:54:10 +0100

    pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,18 @@
 import MySQLdb
 import sys
 
+
 def main():
     """main method excute from queryring states from database"""
     args = sys.argv[1:]
     db = MySQLdb.connect(host="localhost", port=3306, user=f"{args[0]}",
-                        passwd=f"{args[1]}", db=f"{args[2]}", charset="utf8")
+                         passwd=f"{args[1]}", db=f"{args[2]}", charset="utf8")
     cursor = db.cursor()
     cursor.execute("select * from states ORDER BY states.id")
     query_rows = cursor.fetchall()
     for row in query_rows:
         print(row)
+
+
 if __name__ == "__main__":
     main()
