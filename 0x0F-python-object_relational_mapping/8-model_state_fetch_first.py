978ee90226662027f5b0504c8a5c3185c93a0ae8
flowsar <brahim.sar151@gmail.com>
2024-06-23 10:04:00 +0100

    fetch first roe

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""this module is for connecting to mysql database
+    and fetching all data from state table
+    by using sqlalchemy module
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def main():
+    """this function is for executing this the code for
+    fetching first row from states table"""
+    args = sys.argv[1:]
+    passwrd = args[1]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(args[0], passwrd,
+                                   args[2]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = sessionmaker(bind=engine)()
+    states = session.query(State).all()
+    if (states):
+        print(f"{states[0].id}: {states[0].name}")
+
+
+if __name__ == '__main__':
+    main()
