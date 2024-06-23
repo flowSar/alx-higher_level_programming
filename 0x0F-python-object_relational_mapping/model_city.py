2bc6f55dc9d0b1cf7dde129a6e4f98031fd81c75
flowsar <brahim.sar151@gmail.com>
2024-06-23 12:50:15 +0100

    style

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -16,7 +16,7 @@ class City(Base):
     id = Column(Integer, primary_key=True,
                 unique=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False )
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
 
 if __name__ == '__main__':
