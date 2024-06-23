581a1bf8766163b71cdf28a8106bfebe7b023d3a
flowsar <brahim.sar151@gmail.com>
2024-06-23 11:29:59 +0100

    insert new state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python3
+"""this module is for connecting to mysql database
+    and and inserting new state to states table
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def main():
+    """
+        this function if for querying data from table states based on
+        constrint we set: in this case with same the name
+        and by using sqlalchemy instead or raw SQL.
+    """
+    args = sys.argv[1:]
+    passwrd = args[1]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(args[0], passwrd,
+                                   args[2]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    new_state = State(name='Louisiana')
+    session = sessionmaker(bind=engine)()
+    session.add(new_state)
+    session.commit()
+
+
+if __name__ == '__main__':
+    """execute main"""
+    main()
