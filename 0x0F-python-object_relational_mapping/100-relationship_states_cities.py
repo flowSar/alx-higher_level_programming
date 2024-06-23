bb9ca65b22018d6664e932506aad6cabdd75045b
flowsar <brahim.sar151@gmail.com>
2024-06-23 19:52:22 +0100

    states and city relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,1 +1,37 @@
 #!/usr/local/bin/python3
+
+"""
+    this module is for adding one state and one city
+    and linking them with each other
+"""
+from model_city import Base, City
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def main():
+    """this function is for addting one state and one city
+        and linking them with each other
+    """
+    args = sys.argv[1:]
+    passwrd = args[1]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(args[0], passwrd, args[2]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = sessionmaker(bind=engine)()
+
+    state = State(name='California')
+    city = City(name='San Francisco', state_id=1)
+    session.add(state)
+    session.add(city)
+    session.commit()
+
+
+if __name__ == '__main__':
+    """execute main function"""
+    main()
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,1 +1,28 @@
 #!/usr/local/bin/python3
+
+"""this module is for connecting to mysql database
+by using sqlalchemy module"""
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class inherentend from Base and he's
+    representing the database structure"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                unique=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    passwrd = args[1]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(args[0], passwrd,
+                                   args[2]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
