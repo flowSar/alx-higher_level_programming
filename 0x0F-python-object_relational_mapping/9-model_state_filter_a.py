2b0ec24da2cc34cb52339415e3380aa39f57b5ad
flowsar <brahim.sar151@gmail.com>
2024-06-23 11:04:01 +0100

    fetch states that has a in their name

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""this module is for connecting to mysql database
+    and fetching all data from state table
+    by using sqlalchemy module
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def main():
+    """
+        this function if for querying data from table states based on
+        constrint we set
+        and by using sqlalchemy instead or raw SQL.
+    """
+    args = sys.argv[1:]
+    passwrd = args[1]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(args[0], passwrd,
+                                   args[2]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = sessionmaker(bind=engine)()
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+
+if __name__ == '__main__':
+    """execute main"""
+    main()
