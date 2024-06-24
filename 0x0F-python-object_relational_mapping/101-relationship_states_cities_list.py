369bd2479417b554c0c72dcb85065e3c8590f93e
flowsar <brahim.sar151@gmail.com>
2024-06-24 08:19:24 +0100

    List relationship

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+    this module is for query data from two tables that have
+    a relationship with ech other
+"""
+from relationship_city import Base, City
+from relationship_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+def main():
+    """
+        this module is for query data from two tables that have
+        a relationship and print this data
+    """
+    args = sys.argv[1:]
+    user = args[0]
+    passwd = args[1]
+    db = args[2]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user, passwd, db))
+
+    session = sessionmaker(bind=engine)()
+    Base.metadata.create_all(engine)
+    
+    states = session.query(State).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        cities = session.query(City).filter(state.id == City.state_id).all()
+        for city in cities:
+            print(f"    {city.id}: {city.name}")
+
+
+    session.close()
+
+if __name__ == '__main__':
+    main()
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -17,7 +17,8 @@ class City(Base):
     id = Column(Integer, primary_key=True,
                 unique=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id', ondelete='CASCADE'))
+    state_id = Column(Integer, ForeignKey('states.id',
+                                          ondelete='CASCADE'), nullable=False)
 
 
 if __name__ == '__main__':
