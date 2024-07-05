400dcf3fc935c834ff99620520ab584637d7d92e
flowsar <brahim.sar151@gmail.com>
2024-07-05 17:18:25 +0100

    post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""""""
+"""POST request"""
 import urllib.request
 import sys
 
 
-
 def main():
-    """"""
+    """POST reuest to the giving url with email as parameter"""
     url = sys.argv[1:][0]
-    #try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-    #except urllib.error.HTTPError as e:
-        #pass
-        
+    email = sys.argv[1:][1]
+    data = {'email': email}
+    try:
+        encoded_data = urllib.parse.urlencode(data).encode()
+        req = urllib.request.Request(url, encoded_data)
+        with urllib.request.urlopen(req) as response:
+            body = response.read().decode('utf-8')
+            print(f"Your email is: {body}")
+
+    except urllib.error.HTTPError as e:
+        pass
+
 
 if __name__ == '__main__':
     main()
