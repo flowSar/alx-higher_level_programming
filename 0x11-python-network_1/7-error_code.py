b142656888ab22bcf44bd95bd1ea9ebf127b9db7
flowsar <brahim.sar151@gmail.com>
2024-07-05 18:07:58 +0100

    up

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,9 @@ def main():
     url = sys.argv[1:][0]
     try:
         re = requests.get(url)
-        if re.status_code >= 400:
+        if re.status_code == 200:
+            print(re.text)
+        elif re.status_code >= 400:
             print(f"Error code: {re.status_code}")
     except requests.RequestException as e:
         pass
