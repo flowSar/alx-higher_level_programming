0812452b5235ad41df5ee9bfc88907cfd002010d
flowsar <brahim.sar151@gmail.com>
2024-07-05 17:36:48 +0100

    exception

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,9 +7,12 @@ import sys
 def main():
     """fetch request id"""
     url = sys.argv[1:][0]
-    re = requests.get(url)
-    request_id = re.headers['X-Request-Id']
-    print(request_id)
+    try:
+        re = requests.get(url)
+        request_id = re.headers['X-Request-Id']
+        print(request_id)
+    except requests.RequestException as e:
+        pass
 
 
 if __name__ == '__main__':
