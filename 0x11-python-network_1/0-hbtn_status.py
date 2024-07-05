f46504eeff2e3e8693d6ed5c80112aefa6dade1c
flowsar <brahim.sar151@gmail.com>
2024-07-05 15:35:27 +0100

    up

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,14 @@
 """fetch URL by using urllib"""
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    print("Body response:")
-    print("   - type: {}".format(type(body)))
-    print("   - content: {}".format(body))
-    print("   - utf8 content: {}".format(body.decode('utf-8')))
+main():
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("   - type: {}".format(type(body)))
+        print("   - content: {}".format(body))
+        print("   - utf8 content: {}".format(body.decode('utf-8')))
+
+if __name__ == '__main__':
+    main()
