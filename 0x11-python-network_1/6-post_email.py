5f8c918a07038cda89de6aea15197fd4b4120318
flowsar <brahim.sar151@gmail.com>
2024-07-05 18:05:28 +0100

    print status code >= 400

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -17,5 +17,4 @@ def main():
 
 
 if __name__ == '__main__':
-    """execute main function"""
     main()
---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""status code """
+import requests
+import sys
+
+
+def main():
+    """print status code if it's >= 400"""
+    url = sys.argv[1:][0]
+    try:
+        re = requests.get(url)
+        if re.status_code >= 400:
+            print(f"Error code: {re.status_code}")
+    except requests.RequestException as e:
+        pass
+
+
+if __name__ == '__main__':
+    main()
