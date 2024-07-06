4049e2ded7ff421e1c74872207a176370fae0d60
flowsar <brahim.sar151@gmail.com>
2024-07-06 13:38:06 +0100

    up

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,15 +10,13 @@ def main():
     username = sys.argv[1:][0]
     password = sys.argv[1:][1]
     url = 'https://api.github.com/user'
-    try:
-        re = requests.post(url, auth=(username, password))
-        if re.status_code == 200:
-            user_data = re.json()
-            print(f'{user_data['id']}')
-        else:
+    re = requests.post(url, auth=(username, password))
+    if re.status_code == 200:
+        json_data = re.json()
+        if len(json_data) == 0:
             print(None)
-    except Exception as e:
-        pass
+        else:
+            print(json_data['id'])
 
 
 if __name__ == '__main__':
