4020dcb814a2dac663a9f5d90730f6122813ee29
flowsar <brahim.sar151@gmail.com>
2024-07-06 13:28:32 +0100

    up

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,16 +9,14 @@ def main():
     """
     username = sys.argv[1:][0]
     password = sys.argv[1:][1]
-    url = 'https://api.github.com/octocat'
-    headers = {
-        'Authorization': f'Bearer {password}',
-        'X-GitHub-Api-Version': '2022-11-28'
-    }
+    url = 'https://api.github.com/user'
     try:
-        re = requests.get(url, headers=headers)
+        re = requests.post(url, auth=(username, password))
         if re.status_code == 200:
             user_data = re.json()
             print(f'{user_data['id']}')
+        else:
+            print(None)
     except Exception as e:
         pass
 
