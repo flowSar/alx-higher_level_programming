f783df90d1e0b98f51ba6c8ef5708acb79f6416d
flowsar <brahim.sar151@gmail.com>
2024-07-06 15:28:51 +0100

    featch commits sha and owner

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,17 +3,19 @@
 import requests
 import sys
 
+
 def main():
     """use Github API to get fetch commits owner and sha
     """
     owner = sys.argv[1:][0]
     repo = sys.argv[1:][1]
-    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
     re = requests.get(url)
     data = re.json()
     for json_data in data:
         author_name = json_data['commit']['author']['name']
         print(f"{json_data['sha']} {author_name}")
+
 
 if __name__ == '__main__':
     main()
