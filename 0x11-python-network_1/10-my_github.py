02d2232d8b1f82f4000f0292e201f688c63e1f68
flowsar <brahim.sar151@gmail.com>
2024-07-06 11:14:46 +0100

    add exception handler block

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,10 +10,13 @@ def main():
     username = sys.argv[1:][0]
     password = sys.argv[1:][1]
     url = 'https://api.github.com/user'
-    re = requests.post(url, auth=HTTPBasicAuth(username, token))
-    if re.status_code == 200:
-        user_data = re.json()
-        print(f'user_data{user_data['id']}')
+    try:
+        re = requests.post(url, auth=HTTPBasicAuth(username, token))
+        if re.status_code == 200:
+            user_data = re.json()
+            print(f'user_data{user_data['id']}')
+    except Exception as e:
+        pass
 
 
 if __name__ == '__main__':
