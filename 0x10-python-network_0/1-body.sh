61baed36cdafee43ec801fd857160bf67946183d
flowsar <brahim.sar151@gmail.com>
2024-07-07 10:15:43 +0100

    update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response
-curl -s -L -o /dev/null -w "%{html_body}" "$1" | grep -q "^200 " && cat || true
+if [ $(curl -s -o /dev/null -w "%{http_code}" http://$1) -eq 200 ]; then curl http://$1; fi
