4a5342d4d71f4e5d004f636bd3ed9b46fab739c5
flowsar <brahim.sar151@gmail.com>
2024-07-30 17:38:06 +0100

    fetch and write to file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,21 @@
- 
+#!/usr/bin/node
+// fetch urlApi body and write it to a file
+
+const request = require('request');
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+const urlApi = args[0];
+const fileName = args[1];
+
+request.get(urlApi, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(fileName, body, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
