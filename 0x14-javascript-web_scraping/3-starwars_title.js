c72cf6057031a75715d05f1d604c3d3e692c108c
flowsar <brahim.sar151@gmail.com>
2024-07-30 16:49:30 +0100

    fetch json data from API

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,16 @@
- 
+#!/usr/bin/node
+// fetch json data from API
+
+const request = require('request');
+
+const args = process.argv.slice(2);
+const id = args[0];
+
+const urlApi = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request.get(urlApi, { json: true }, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  console.log(`${body.title}`);
+});
