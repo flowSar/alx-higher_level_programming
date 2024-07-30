425bcd83ca002cc989a5bbaed0520d0d673b0c2d
flowsar <brahim.sar151@gmail.com>
2024-07-30 15:05:43 +0100

    get response code using request module

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,15 @@
- 
+#!/usr/bin/node
+// get response code
+
+const request = require('request');
+
+const args = process.argv.slice(2);
+const url = args[0];
+
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+  }
+  console.log(`code: ${response.statusCode}`);
+});
