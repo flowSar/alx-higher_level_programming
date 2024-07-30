93eeca644cbc841ed7702115e683834fc7a2d5cb
flowsar <brahim.sar151@gmail.com>
2024-07-30 18:00:04 +0100

    calculate completer task for each user

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,24 @@
- 
+#!/usr/bin/node
+// fetch urlApi body and calculate completer task for each user
+
+const request = require('request');
+
+const args = process.argv.slice(2);
+const urlApi = args[0];
+
+const data = {};
+
+request.get(urlApi, { json: true }, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  for (const user of body) {
+    if (isNaN(data[user.userId])) {
+      data[user.userId] = 0;
+    }
+    if (user.completed === true) {
+      data[user.userId] = data[user.userId] + 1;
+    }
+  }
+  console.log(data);
+});
