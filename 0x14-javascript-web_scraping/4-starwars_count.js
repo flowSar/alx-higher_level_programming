766d25c96e6c9167e771f1153e66296bc29d1f88
flowsar <brahim.sar151@gmail.com>
2024-07-30 17:13:01 +0100

    counter number of movies

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,22 @@
- 
+#!/usr/bin/node
+// count number of movies where the character “Wedge Antilles” is present.
+
+const request = require('request');
+
+const args = process.argv.slice(2);
+const urlApi = args[0];
+
+let count = 0;
+request.get(urlApi, { json: true }, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  for (const item of body.results) {
+    for (const str of item.characters) {
+      if (str.includes('18')) {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+});
