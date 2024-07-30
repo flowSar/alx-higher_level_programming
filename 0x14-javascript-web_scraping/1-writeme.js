84e9333fef93c1fcd1f5ea8de049080552098273
flowsar <brahim.sar151@gmail.com>
2024-07-30 12:49:38 +0100

    write string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,13 @@
- 
+#!/usr/bin/node
+// write string to a file
+
+const fs = require('fs');
+const args = process.argv.slice(2);
+const fileName = args[0];
+const stringContent = args[1];
+
+fs.writeFile(fileName, stringContent, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
