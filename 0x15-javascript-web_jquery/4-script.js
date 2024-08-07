c5685e4f1a8e34f46fcafdb7258ab4c89795ab55
flowsar <brahim.sar151@gmail.com>
2024-08-07 18:11:08 +0100

    toggles class header

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,1 +1,24 @@
- 
+
+$(function() {
+    let redFound = $('header').hasClass('red')
+    let greenFound = $('header').hasClass('green')
+
+    if (redFound && greenFound) {
+        $('header').removeClass('red')
+    } 
+    if (!redFound && !greenFound) {
+        $('header').addClass('red')
+    }
+    $('#toggle_header').click(function () {
+        redFound = $('header').hasClass('red')
+        greenFound = $('header').hasClass('green')
+        
+        if (redFound) {
+            $('header').removeClass('red')
+            $('header').addClass('green')
+        } else if (greenFound) {
+            $('header').removeClass('green')
+            $('header').addClass('red')
+        }
+    });
+});
