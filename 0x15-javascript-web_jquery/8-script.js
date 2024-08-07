f2fc31acad1edb4e4225447f66565bf4f7d3f416
flowsar <brahim.sar151@gmail.com>
2024-08-07 18:31:54 +0100

    fetch a movies titles from API and list them

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,9 @@
- 
+
+$(function() {
+    $.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+        results = data['results'];
+        for (const result of results) {
+            $('#list_movies').append(`<li>${result['title']}</li>`)
+        }
+    })
+});
