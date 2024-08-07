c1772010d57652c2d559a70e6805ec74966495d3
flowsar <brahim.sar151@gmail.com>
2024-08-07 17:34:48 +0100

    click event jQuery

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,6 @@
 
-document.querySelector("#red_header").addEventListener('click', () => {
-   document.querySelector('header').style.color = '#FF0000'; 
-});
+$(function() {
+    $('#red_header').click(function () {
+        $('header').css('color', '#FF0000');
+    });
+}); 
